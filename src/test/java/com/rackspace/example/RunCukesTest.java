2f8b22e469325472f1fb85586f2811acd77ea836
David Kowis <david_kowis@homedepot.com>
2015-09-05 13:42:10 -0500

    Fixing api changes

--- src/test/java/com/rackspace/example/RunCukesTest.java
+++ src/test/java/com/rackspace/example/RunCukesTest.java
@@ -1,9 +1,10 @@
 package com.rackspace.example;
 
-import cucumber.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(features = "classpath:", glue = {"src/test/resources", "classpath:cucumber/examples/java/calculator"})
+@CucumberOptions(features = "classpath:", glue = {"src/test/resources", "classpath:cucumber/examples/java/calculator"})
 public class RunCukesTest {
 }
